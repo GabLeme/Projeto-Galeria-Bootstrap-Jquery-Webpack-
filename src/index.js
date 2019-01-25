f41a91f5c5f7b575d67fd2f1d97aba445d18daf1
Gabriel Leme <gabriel.leme@digisystem.com.br>
2019-01-25 11:26:55 -0200

    Adicionando Header  e criando Includes.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Arquivos SASS
+import './scss/index.scss';
+
+//Dependencias
+import 'jquery';
+import 'bootstrap';
+
+//Referenciando includes.js com caminho relativo;
+import './js/core/includes'
